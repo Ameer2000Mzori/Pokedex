fbcee2b87f077ca80b341d13d90e01666524273c
Ameer <ameerapex496@gmail.com>
2023-12-10 22:44:33 +0100

    added apikey / fetch data function

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,20 @@
 // selecting elements
 
 // our api key / global varibales
+const API_KEY = `https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`;
 
 //our data fetch api function
+async function getData(Key) {
+  try {
+    const response = await fetch(Key);
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // our fucntions
 
 // our eventlisnters
+getData(API_KEY);
